cd8b04a148c6c8be4d657789380fd738e7968968
Bockwurst2016 <102362045+Bockwurst2016@users.noreply.github.com>
2022-05-06 11:50:49 +0200

    Update BungalwSoftware.java

--- BungalwSoftware.java
+++ BungalwSoftware.java
@@ -1,54 +1,45 @@
+
 /**
- *  VORLAGE GUI 
+ *  Reservierungssoftware 
  *  ---------------------------------------------------------------------- 
  *  Autor: Bockwurst2016
  *  Erstellungs-Datum: 25.03.2022
- *  Version 1.1
- *  Datum der letzten Aenderung: 05.04.2022
+ *  Version 1.2
+ *  Datum der letzten Aenderung: 06.05.2022
  *  ----------------------------------------------------------------------
  */
 
-import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.awt.Frame;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.border.EmptyBorder;
-
-import org.eclipse.ui.IFileEditorMapping;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import java.awt.GridBagLayout;
-import java.awt.Dimension;
-import javax.swing.JComboBox;
-import javax.swing.DefaultComboBoxModel;
 
-public class BungalwSoftware extends JFrame {
+
+
+public class BungalwSoftwareFinal extends JFrame {
 	private JTextField textBungalowNr;
 	private JTextField textStartWoche;
 	private JTextField textEndWoche;
 	private JTextField textAnzahlPersonen;
 	private JTextField textRueckmeldung;
 	private JTextArea textBuchungenAnzeigen;
+	boolean ausgebucht = false;
+
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		
-		
-	
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					BungalwSoftware frame = new BungalwSoftware();
+					BungalwSoftwareFinal frame = new BungalwSoftwareFinal();
 					frame.setVisible(true);
 					frame.pack();
 				} catch (Exception e) {
@@ -61,115 +52,125 @@ public class BungalwSoftware extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public BungalwSoftware() {
-	
+	public BungalwSoftwareFinal() {
+		setTitle("Reservirungen");
+
 		int[][] Buchungen = new int[11][4];
 		
-		setAutoRequestFocus(false);
+
+		setAutoRequestFocus(true);
 		setPreferredSize(new Dimension(800, 450));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		
-		boolean reservierungDurchfuehren = false;
-		
+
 		JButton btnReservierungDurchfuehren = new JButton("Reservierung Durchf\u00FChren");
-		btnReservierungDurchfuehren.setBounds(27, 250, 200, 60);
+		btnReservierungDurchfuehren.setBounds(64, 283, 200, 60);
 		btnReservierungDurchfuehren.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(e.getSource()==btnReservierungDurchfuehren) {
+				if (e.getSource() == btnReservierungDurchfuehren) {
+
+					if (ausgebucht == false) {
+						Buchungen[Integer.parseInt(textBungalowNr.getText())][0] = Integer.parseInt(textBungalowNr.getText());
+						Buchungen[Integer.parseInt(textBungalowNr.getText())][1] = Integer.parseInt(textStartWoche.getText());
+						Buchungen[Integer.parseInt(textBungalowNr.getText())][2] = Integer.parseInt(textEndWoche.getText());
+						Buchungen[Integer.parseInt(textBungalowNr.getText())][3] = Integer.parseInt(textAnzahlPersonen.getText());
 					
-			 Buchungen[Integer.parseInt(textBungalowNr.getText())][0]= Integer.parseInt(textBungalowNr.getText());
-			 Buchungen[Integer.parseInt(textBungalowNr.getText())][1]= Integer.parseInt(textStartWoche.getText());
-			 Buchungen[Integer.parseInt(textBungalowNr.getText())][2]= Integer.parseInt(textEndWoche.getText());
-			 Buchungen[Integer.parseInt(textBungalowNr.getText())][3]= Integer.parseInt(textAnzahlPersonen.getText());
+					}
+					
+					textBungalowNr.setText(null);
+					textStartWoche.setText(null);
+					textEndWoche.setText(null);
+					textAnzahlPersonen.setText(null);
+					
+					for (int i = 1; i < Buchungen.length; i++) {
+						for (int j = 1; j < Buchungen.length; j++) {
+							if (Buchungen[i][0] == Buchungen[j][Integer.parseInt(textBungalowNr.getText())]) {
+							 ausgebucht = true;	
+							}
+						}
+					}
+					if (ausgebucht == true ) {
+						textBuchungenAnzeigen.setText("AUSGEBUCHT!!!!!");
+					}
+					
 				}
-				
-//				if (e.getSource()==) {
-//					
-//				}
+
 			}
+
+			
 		});
 		getContentPane().setLayout(null);
 		getContentPane().add(btnReservierungDurchfuehren);
-		
-		
-		
+
 		textBungalowNr = new JTextField();
 		textBungalowNr.setBounds(159, 21, 164, 40);
 		getContentPane().add(textBungalowNr);
 		textBungalowNr.setColumns(10);
-		
+
 		textStartWoche = new JTextField();
 		textStartWoche.setBounds(159, 72, 164, 40);
 		textStartWoche.setColumns(10);
 		getContentPane().add(textStartWoche);
-		
+
 		JLabel lblBungalowNr = new JLabel("Bungalow-Nummer:");
 		lblBungalowNr.setBounds(27, 21, 100, 40);
 		getContentPane().add(lblBungalowNr);
-		
+
 		JLabel lblStartWoche = new JLabel("Start-Woche:");
 		lblStartWoche.setBounds(27, 72, 100, 40);
 		getContentPane().add(lblStartWoche);
-		
+
 		JLabel lblEndWoche = new JLabel("End-Woche:");
 		lblEndWoche.setBounds(27, 123, 100, 40);
 		getContentPane().add(lblEndWoche);
-		
+
 		textEndWoche = new JTextField();
 		textEndWoche.setColumns(10);
 		textEndWoche.setBounds(159, 123, 164, 40);
 		getContentPane().add(textEndWoche);
-		
+
 		textAnzahlPersonen = new JTextField();
 		textAnzahlPersonen.setColumns(10);
 		textAnzahlPersonen.setBounds(159, 173, 164, 40);
 		getContentPane().add(textAnzahlPersonen);
-		
+
 		JLabel lblAnzahlPersonen = new JLabel("Anzahl Personen:");
 		lblAnzahlPersonen.setBounds(27, 174, 100, 40);
 		getContentPane().add(lblAnzahlPersonen);
-		
+
 		textRueckmeldung = new JTextField();
 		textRueckmeldung.setColumns(10);
 		textRueckmeldung.setBounds(565, 20, 120, 40);
 		getContentPane().add(textRueckmeldung);
-		
+
 		JLabel lblAuswahlBungalowAnzeige = new JLabel("Bungalow ausw\u00E4hlen:");
 		lblAuswahlBungalowAnzeige.setBounds(425, 20, 120, 40);
 		getContentPane().add(lblAuswahlBungalowAnzeige);
-		
+
 		JButton btnAuswertung = new JButton("Auswertung");
-		btnAuswertung.setBounds(460, 300, 120, 60);
+		btnAuswertung.setBounds(522, 302, 120, 60);
 		btnAuswertung.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				
-				if(e.getSource()==btnAuswertung) {
-					
-					textBuchungenAnzeigen.setText("Bungalo Nr." +  Buchungen[Integer.parseInt(textBungalowNr.getText())][0] + 
-					"\n" + "Startwoche: " +Buchungen[Integer.parseInt(textBungalowNr.getText())][1] +
-					"\n" + "Endwoche: " +Buchungen[Integer.parseInt(textBungalowNr.getText())][2] + 
-					"\n" + "Anzahl Personen: " +Buchungen[Integer.parseInt(textBungalowNr.getText())][3]);
-					
-					 
-						}
+
+				if (e.getSource() == btnAuswertung) {
+
+					textBuchungenAnzeigen
+							.setText("Bungalo Nr." + Buchungen[Integer.parseInt(textRueckmeldung.getText())][0] + "\n"
+									+ "Startwoche: " + Buchungen[Integer.parseInt(textRueckmeldung.getText())][1] + "\n"
+									+ "Endwoche: " + Buchungen[Integer.parseInt(textRueckmeldung.getText())][2] + "\n"
+									+ "Anzahl Personen: " + Buchungen[Integer.parseInt(textRueckmeldung.getText())][3]);
+
+				}
 			}
 		});
 		getContentPane().add(btnAuswertung);
-	
-		
+
 		textBuchungenAnzeigen = new JTextArea();
 		textBuchungenAnzeigen.setColumns(10);
-		textBuchungenAnzeigen.setBounds(425, 90, 200, 200);
+		textBuchungenAnzeigen.setBounds(480, 90, 200, 200);
 		getContentPane().add(textBuchungenAnzeigen);
-	
-				
-		
-	
+
 	}
-	
-	
-	
-	
-	
+
 }
